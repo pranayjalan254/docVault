1b5e308bc156e203f6ea8ed25ca22c364ad88fa2
Pranay <pranayjalan.work@gmail.com>
2024-08-23 22:31:01 +0530

    making hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
+import Hero from "./components/Hero/hero";
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero/hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section id="home" className="hero-section">
+      <div className="hero-content">
+        <h2>Unlock the Future of Credential Verification</h2>
+        <p>Decentralized. Trustless. Seamless.</p>
+        <button className="cta-button">Explore Now</button>
+      </div>
+      <div className="hero-animation">
+        <img src="src/assets/hero.webp" alt="Hero Animation" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
