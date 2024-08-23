2ce343cbe0ba1db2f6f8e08d37cb00f0f31f0b9a
Pranay <pranayjalan.work@gmail.com>
2024-08-23 22:15:14 +0530

    updating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar/navbar";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src="/src/assets/logo.png" alt="DocVault Logo" />
+        <h1>DocVault</h1>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <button className="cta-button">Get Started</button>
+    </nav>
+  );
+};
+
+export default Navbar;
