082f809917d56fed87cbbb5832df6943e1dffe95
Pranay <pranayjalan.work@gmail.com>
2024-08-31 12:19:59 +0530

    issue credential functionality completed

--- src/parse.js
+++ src/parse.js
@@ -1,10 +1,11 @@
 // parse.js
 export async function loadData() {
   try {
-    const response = await fetch("/data.json");
+    const response = await fetch("http://localhost:5000/api/metadata");
+    console.log(response);
     const data = await response.json();
 
-    const WALLET_ADDRESS = data.walletaddress;
+    const WALLET_ADDRESS = data.walletAddress;
     const COURSE = data.course;
     const DATE = data.date;
 
