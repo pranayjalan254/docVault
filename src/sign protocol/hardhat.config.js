de42973c76206ad27c99def57ff6433e36d93eef
Pranay <pranayjalan.work@gmail.com>
2024-09-01 22:40:15 +0530

    securing private key

--- src/sign protocol/hardhat.config.js
+++ src/sign protocol/hardhat.config.js
@@ -1,5 +1,5 @@
 const { getUsedIdentifiers } = require("typechain");
-require("@openzeppelin/hardhat-upgrades")
+require("@openzeppelin/hardhat-upgrades");
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
@@ -16,30 +16,21 @@ module.exports = {
     },
     sepolia: {
       url: "https://rpc.sepolia.org",
-      accounts: ["7c6dcc5c7d55f6b35be37f7b9b6094c684156a34f9fe18dc1e1a6f7837d572a4"],
+      accounts: ["process.env.VITE_PRIVATE_KEY"],
       chainId: 11155111,
       blockConfirmations: 1,
     },
-    sapphire: {
-      url: "https://sapphire-devnet.oasis.dev",
-      accounts: ["7c6dcc5c7d55f6b35be37f7b9b6094c684156a34f9fe18dc1e1a6f7837d572a4"],
-      chainId: 23295,
-      blockConfirmations: 1,
-    },
   },
-  gasReporter:{
-    enabled : true,
-    currency : "usd",
+  gasReporter: {
+    enabled: true,
+    currency: "usd",
   },
   solidity: {
-    compilers: [
-      { version: "0.8.0" },
-      { version: "0.8.24" },
-    ],
+    compilers: [{ version: "0.8.0" }, { version: "0.8.24" }],
   },
   namedAccounts: {
     deployer: {
-      default: 0, // Here this will by default take the first account as deployer
+      default: 0,
     },
   },
 };
